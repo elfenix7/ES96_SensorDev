9f9092a7cf78c3067d3aec4e49acc69270acf4ff
Matthew Giles <matthew@giles-r.us>
2020-04-19 20:06:21 -0700

    fixed baud rate

--- PiScript/datalogger.py
+++ PiScript/datalogger.py
@@ -12,7 +12,7 @@ INIT_SUCCESS = "Good to go!"
 
 ser = serial.Serial (
         port='/dev/ttyUSB0',
-        baudrate=9600,
+        baudrate=115200,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS
